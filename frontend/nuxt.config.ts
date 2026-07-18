3ae2eea608f1c6848d4b690165e3dab07a78407c
SNC MIS <sncmis@SNCs-Mac-mini.local>
2026-07-18 11:51:29 +0800

    Nuxt + vuetify and Strapi setup

---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,35 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-12-21',
+  devtools: { enabled: true },
+
+  // ssr: false,
+  modules: ['@nuxt/fonts', 'vuetify-nuxt-module', '@nuxt/eslint'],
+
+  app: {
+    head: {
+      link: [
+        { rel: 'stylesheet', href: '/layers.css' },
+      ],
+    },
+  },
+
+  vuetify: {
+    moduleOptions: {
+      styles: { configFile: 'assets/styles/settings.scss' },
+    },
+    vuetifyOptions: {
+      theme: {
+        defaultTheme: 'dark', // default 'system' requires `ssr: false` to avoid hydration warnings
+      },
+    },
+  },
+
+  eslint: {
+    config: {
+      import: {
+        package: 'eslint-plugin-import-lite',
+      },
+    },
+  },
+})
